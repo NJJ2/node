44beef9e632549ffe83fbc708f4bbc67e9708432
NaveenJ <naveenjayaram04@gmail.com>
2019-07-23 23:31:54 +0530

    lessons 9 and 10  Please enter the commit message for your changes. Lines starting

---
+++ app.js
@@ -0,0 +1,14 @@
+/* lesson 9
+const fs = require("fs")
+fs.writeFileSync("notes.txt", "My name")
+fs.appendFileSync("notes.txt", "My name is name \n")
+*/
+
+/*  Lesson 10  */
+  // Challenge
+  const getNotes = require("./notes.js");
+  const notes = getNotes()
+  console.log(notes);
+/* end lesson 10 */
+
+  
---
+++ notes.js
@@ -0,0 +1,7 @@
+// Lesson 10 Challenge
+const getNotes = function () {
+  return "Your notes..."
+}
+// Lesson 10 Challenge
+
+module.exports = getNotes
